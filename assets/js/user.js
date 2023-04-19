6da2024d2a023b5692ea6a82f0e269fdca4c41d5
diddyjax19 <adetobi59@gmail.com>
2023-04-19 22:17:10 +0000

    Changes made and code added

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,5 +1,33 @@
 let user_name = sessionStorage.getItem("name");
+user_name = sanitizeInput(user_name);
+
 let user_points = sessionStorage.getItem("points");
+user_points = sanitizeInput(user_points);
+
+if (!isValidName(user_name)) {
+    alert("Invalid name. Please enter a valid name.");
+    window.location.href = "index.html";
+}
+
 
 document.querySelector("span.name").innerHTML = user_name;
 document.querySelector("span.points").innerHTML = user_points;
+
+
+
+
+function sanitizeInput(input) {
+    
+    return input.replace(/<[^>]*>/g, "");
+}
+
+
+function isValidName(name) {
+ 
+    return /^[a-zA-Z]{3,20}$/.test(name);
+}
+
+
+function isValidSession() {
+ 
+}
